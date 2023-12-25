61bafbef97b79b9c625b413728823b8b71de17b1
Khushboo9636 <103207231+Khushboo9636@users.noreply.github.com>
2023-12-25 21:35:56 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,7 @@ dotenv.config()
 const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
-    methods: {"GET","HEAD","PUT","PATCH","POST","DELETE"},
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }));
 
